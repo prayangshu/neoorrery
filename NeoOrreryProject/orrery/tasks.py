a4deb646374d8c62914c1d4503fafd8efd755543
Prayangshu <prayangshubiswashritwick@gmail.com>
2024-09-28 14:30:57 +0600

    updated requirements

--- NeoOrreryProject/orrery/tasks.py
+++ NeoOrreryProject/orrery/tasks.py
@@ -1,22 +1,17 @@
 # orrery/tasks.py
 
 from celery import shared_task
-from .management.commands.fetch_nasa_data import Command as FetchNASADataCommand
 from .management.commands.fetch_real_time_close_approaches import Command as FetchRealTimeCloseApproachesCommand
 from .models import NasaDataLog
 
 @shared_task
-def fetch_nasa_data_task():
-    """Fetch NASA data using the fetch_nasa_data management command."""
-    fetch_nasa_data = FetchNASADataCommand()
-    fetch_nasa_data.handle()
-    # Log the action
-    NasaDataLog.objects.create(user=None, action='Fetched NASA data')
+def update_real_time_close_approaches(user_id=None):
+    """Fetch real-time close approaches and log the action."""
+    fetch_real_time_approaches = FetchRealTimeCloseApproachesCommand()
+    fetch_real_time_approaches.handle()
 
-@shared_task
-def update_real_time_close_approaches():
-    """Fetch real-time close approaches using the fetch_real_time_close_approaches management command."""
-    fetch_command = FetchRealTimeCloseApproachesCommand()
-    fetch_command.handle()
-    # Log the action
-    NasaDataLog.objects.create(user=None, action='Updated Real-Time Close Approaches')
+    user = None
+    if user_id:
+        user = User.objects.get(id=user_id)
+
+    NasaDataLog.objects.create(user=user, action='Updated Real-Time Close Approaches')

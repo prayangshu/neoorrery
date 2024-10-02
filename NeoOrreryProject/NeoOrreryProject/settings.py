56d18d1b9080a0ec8e6cd2198446b931c43092ff
Prayangshu <prayangshubiswashritwick@gmail.com>
2024-10-02 09:50:29 +0600

    update

--- NeoOrreryProject/NeoOrreryProject/settings.py
+++ NeoOrreryProject/NeoOrreryProject/settings.py
@@ -24,10 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'orrery',
     'django_celery_beat',
-<<<<<<< Updated upstream
-=======
     'tinymce',  # Use TinyMCE
->>>>>>> Stashed changes
 ]
 
 # Middleware configuration
